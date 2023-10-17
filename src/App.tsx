03cf03ce34f70a0996f3d7c2d712ecd148293bb6
Ali <dev.alisevinc@gmail.com>
2023-10-17 18:47:12 +0300

    small opt.

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,19 @@ import FriendList from "./components/friends/FriendList";
 import Bill from "./components/bill/Bill";
 
 const DATA = [
-  { name: "Lara", image: "https://i.pravatar.cc/48", id: "f1", stats: 0 },
-  { name: "Fink", image: "https://i.pravatar.cc/48", id: "f2", stats: -10 },
-  { name: "Clark", image: "https://i.pravatar.cc/48", id: "f3", stats: 10 },
+  { name: "Lara", image: "https://i.pravatar.cc/48?=123", id: "f1", stats: 0 },
+  {
+    name: "Fink",
+    image: "https://i.pravatar.cc/48?=132",
+    id: "f2",
+    stats: -10,
+  },
+  {
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?=321",
+    id: "f3",
+    stats: 10,
+  },
 ];
 
 export interface FriendType {
@@ -19,7 +29,6 @@ export interface FriendType {
 function App() {
   const [friends, setFriends] = useState<FriendType[]>(DATA);
   const [showNewFriend, setShowNewFriend] = useState<boolean>(false);
-  const [showBillForm, setShowBillForm] = useState<boolean>(false);
   const [selectedFriend, setSelectedFriend] = useState<FriendType | null>(null);
 
   function handleAddFriend(data: FriendType) {
@@ -27,7 +36,6 @@ function App() {
   }
   function handleToggleShowNewFriend() {
     setShowNewFriend((prev) => !prev);
-    setShowBillForm(false);
     setSelectedFriend(null);
   }
 
@@ -35,10 +43,8 @@ function App() {
     const selected = friends.filter((friend) => friend.id === id)[0];
     setShowNewFriend(false);
     if (selectedFriend?.id === id) {
-      setShowBillForm(false);
       setSelectedFriend(null);
     } else {
-      setShowBillForm(true);
       setSelectedFriend(selected);
     }
   }
@@ -67,7 +73,7 @@ function App() {
         showNewFriend={showNewFriend}
         onSelectFriend={handleSelectFriend}
       />
-      {showBillForm && (
+      {selectedFriend !== null && (
         <Bill selectedFriend={selectedFriend} onCalculate={handleCalculate} />
       )}
     </main>
